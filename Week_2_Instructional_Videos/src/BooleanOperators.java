999d4c7d386eb8ddc1dce3858943a7af748680b8
devon <devon.geertsen@gmail.com>
2022-11-05 09:12:17 -0600

    "First push for week 2"

---
+++ Week_2_Instructional_Videos/src/BooleanOperators.java
@@ -0,0 +1,20 @@
+
+public class BooleanOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Equality Operators
+		System.out.println(2 == 2);
+		System.out.println(2>2);
+		System.out.println(3>=9);
+		System.out.println(3>3);
+		//&& and || or
+		System.out.println(2 == 2 && 3>=9);
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(true || false);
+		System.out.println(false || false);
+		
+	}
+
+}
---
+++ Week_2_Instructional_Videos/src/Loops.java
@@ -0,0 +1,10 @@
+
+public class Loops {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(true || false);
+	}
+
+}
