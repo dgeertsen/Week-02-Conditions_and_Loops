8d1838cc9b5c0b7dd54a0b96ff09218e1b74a74c
devon <devon.geertsen@gmail.com>
2022-11-05 10:06:50 -0600

    Videos finished

--- Week_2_Instructional_Videos/src/Conditionals.java
+++ Week_2_Instructional_Videos/src/Conditionals.java
@@ -3,7 +3,64 @@ public class Conditionals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		//if & else
+		int age = 14;
+		if (age >=16)
+		{
+			System.out.println("You can drive");
+			
+		}
+		else
+		{
+			System.out.println("Please wait " + (16-age) + " years.");
+		}
+		//else if
+		double costOfMilk =1.5;
+		if(costOfMilk <=2)
+		{
+			System.out.println("Buying 2");
+		
+		}
+		else if(costOfMilk <=3)
+		{
+			System.out.println("Buying 1");
+		
+		}
+		else 
+		{
+			System.out.println("No milk for you!!!");
+		
+		}		
+		//Switch Case
+		char grade = 'D';
+		switch (grade) {
+		case 'A':
+			System.out.println("90%");
+			break;
+		case 'B':
+			System.out.println("80%");	
+			break;
+		case 'C':
+			System.out.println("70%");
+			break;
+		default:
+			System.out.println("0%");
+		}
+		//Nested
+		if(5==5) {
+			if(4==4) {
+				System.out.println("yes");
+			}
+				
+		}
+		//flatten above statement
+		if(5==5 && 4==4)
+		{
+			System.out.println("yes");
+		}
+		
+	}	
+	
+	
 
 }
--- Week_2_Instructional_Videos/src/Loops.java
+++ Week_2_Instructional_Videos/src/Loops.java
@@ -4,7 +4,34 @@ public class Loops {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(true || false);
+		//For Loops. Know how many times you need to iterate
+		//for(int i = 0; i <10;i++) {
+		//	System.out.println(i);
+		//	//i++ happens post iteration(after code executes)
+		//}	
+		//print even numbers 0 to 10
+		for(int i = 500; i<=100;i++) {
+			if(i%2==0) {
+			System.out.println(i);
+			//i++ happens post iteration(after code executes)
+			}
+		}	
+		//while loop. iterate till condition is met.
+		//condition checked at start
+		//Don't know how much to run
+		int x=10;
+		while(x<10) {
+			System.out.println(x);
+			x++;
+		}
+		//Do While Loop
+		//Will run at least once
+		//Checks condition at end.
+		do
+		{
+			System.out.println(x);
+			x++;
+		}while(x<2); 		
 	}
 
 }
